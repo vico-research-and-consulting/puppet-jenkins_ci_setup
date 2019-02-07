dff2a7b60877b0f337b64e45660d403c1629d66e
Marc Schoechlin <marc.schoechlin@vico-research.com>
2019-02-07 21:53:39 +0100

    Settings

--- manifests/profiles/java.pp
+++ manifests/profiles/java.pp
@@ -1,4 +1,6 @@
 class jenkins_ci_setup::profiles::java (
+    String $maven_settings_template = "",
+    Hash $maven_settings_config     = {},
 ) {
   package{ "maven": }
 
@@ -10,5 +12,21 @@ class jenkins_ci_setup::profiles::java (
   jenkins::plugin { 'gradle': }
   jenkins::plugin { 'jacoco': }
 
+  file { '/var/lib/jenkins/.m2/':
+    ensure => directory,
+    owner  => "jenkins",
+    group  => "jenks",
+    mode   => "0755",
+  }
+  if $maven_settings_template != "" {
+    file { '/var/lib/jenkins/.m2/settings.xml':
+      ensure => file,
+      owner  => "jenkins",
+      group  => "jenkins",
+      mode   => "0644",
+      source => $maven_settings_template, 
+      require => File['/var/lib/jenkins/.m2/'],
+    }
+  }
 }
 
