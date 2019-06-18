48df210d4b52ac848578b0ef31d4a79d38223c7b
Marc Schoechlin <marc.schoechlin@vico-research.com>
2019-06-18 12:34:38 +0200

    add python tooling

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,5 +6,5 @@ class jenkins_ci_setup {
   include ::jenkins_ci_setup::profiles::docker
   include ::jenkins_ci_setup::profiles::haproxy
   include ::jenkins_ci_setup::profiles::java
-
+  include ::jenkins_ci_setup::profiles::python
 }
---
+++ manifests/profiles/python.pp
@@ -0,0 +1,15 @@
+class jenkins_ci_setup::profiles::python (
+    String $maven_settings_template = "",
+    Hash $maven_settings_config     = {},
+) {
+  ensure_packages(
+      [ "python3-pip", "python3-dev", "virtualenv", "python3", "zlib1g-dev", 
+      ]
+  )
+  ensure_packages(['docker-compose', 'pipenv' ], {
+         ensure   => present,
+         provider => 'pip3',
+         require  => [ Package['python3-pip'], ],
+  })
+}
+
