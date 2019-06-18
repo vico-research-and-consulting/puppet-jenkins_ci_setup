98af5ce0b330d026d8994a11ea43469988dba275
Marc Schoechlin <marc.schoechlin@vico-research.com>
2019-06-18 12:50:11 +0200

    added note

--- manifests/profiles/python.pp
+++ manifests/profiles/python.pp
@@ -6,6 +6,7 @@ class jenkins_ci_setup::profiles::python (
       [ "python3-pip", "python3-dev", "virtualenv", "python3", "zlib1g-dev", 
       ]
   )
+ # https://gist.github.com/kogcyc/07c3e5d1f427c9fa6b99044d81f8ee82
   ensure_packages(['docker-compose', 'pipenv' ], {
          ensure   => present,
          provider => 'pip3',
