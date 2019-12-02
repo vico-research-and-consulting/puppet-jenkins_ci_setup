f76331aca00e3ce8a5e3d1e0640f38120bce0078
Marc Schoechlin <marc.schoechlin@vico-research.com>
2019-12-02 11:22:08 +0100

    add jdk

--- manifests/profiles/java.pp
+++ manifests/profiles/java.pp
@@ -28,5 +28,17 @@ class jenkins_ci_setup::profiles::java (
       require => File['/var/lib/jenkins/.m2/'],
     }
   }
+
+  package{'openjdk-11-jdk':
+    ensure => present,
+  }
+  exec { 'update-java-alternatives -s java-1.8.0-openjdk-amd64 -v':
+      path      => '/usr/local/sbin:/usr/local/bin:/bin:/sbin:/usr/bin:/usr/sbin',
+      user      => 'root',
+      logoutput => true,
+      unless    => 'java -version 2>&1 |grep -P \'^openjdk version "1.8.\d+_\d+"\'',
+      require   => Package['openjdk-11-jdk'],
+  }
+
 }
 
