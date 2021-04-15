0b2afb8ac7cc6612ec8573b60f5f03b8e7d8ea91
Jonas Grote <jonas.grote@vico-research.com>
2021-04-15 16:54:50 +0200

    Add black/isort as pip modules, not actual packages

--- manifests/profiles/python.pp
+++ manifests/profiles/python.pp
@@ -3,11 +3,11 @@ class jenkins_ci_setup::profiles::python (
     Hash $maven_settings_config     = {},
 ) {
   ensure_packages(
-      [ "python3-pip", "python3-dev", "virtualenv", "python3", "zlib1g-dev", "black", "isort"
+      [ "python3-pip", "python3-dev", "virtualenv", "python3", "zlib1g-dev"
       ]
   )
  # https://gist.github.com/kogcyc/07c3e5d1f427c9fa6b99044d81f8ee82
-  ensure_packages(['docker-compose', 'pipenv' ], {
+  ensure_packages(['docker-compose', 'pipenv', "black", "isort" ], {
          ensure   => present,
          provider => 'pip3',
          require  => [ Package['python3-pip'], ],
