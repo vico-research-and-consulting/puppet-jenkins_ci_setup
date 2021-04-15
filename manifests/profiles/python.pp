3cd28ef67167b6830ca5d727915ba69d950bbf53
Jonas Grote <jonas.grote@vico-research.com>
2021-04-15 13:53:16 +0200

    Add black and isort

--- manifests/profiles/python.pp
+++ manifests/profiles/python.pp
@@ -3,7 +3,7 @@ class jenkins_ci_setup::profiles::python (
     Hash $maven_settings_config     = {},
 ) {
   ensure_packages(
-      [ "python3-pip", "python3-dev", "virtualenv", "python3", "zlib1g-dev", 
+      [ "python3-pip", "python3-dev", "virtualenv", "python3", "zlib1g-dev", "black", "isort"
       ]
   )
  # https://gist.github.com/kogcyc/07c3e5d1f427c9fa6b99044d81f8ee82
