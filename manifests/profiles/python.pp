1ce381273d129d86c760982039fbf4075502cd04
Jonas Grote <jonas.grote@vico-research.com>
2021-05-04 17:05:31 +0200

    Add twine to be able to build packages

--- manifests/profiles/python.pp
+++ manifests/profiles/python.pp
@@ -6,7 +6,7 @@ class jenkins_ci_setup::profiles::python (
     [ "python3-pip", "python3-dev", "virtualenv", "python3", "zlib1g-dev", ]
   )
   # https://gist.github.com/kogcyc/07c3e5d1f427c9fa6b99044d81f8ee82
-  ensure_packages(['docker-compose', 'pipenv', "black", "isort", ], {
+  ensure_packages(['docker-compose', 'pipenv', "black", "isort", "twine" ], {
     ensure   => present,
     provider => 'pip3',
     require  => [ Package['python3-pip'], ],
