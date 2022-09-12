c9d88e596fffcbc190b8a2cfbed260fd9ba4eab3
Jonas Grote <jonas.grote@vico-research.com>
2022-09-12 09:59:26 +0200

    Remove deprecated maven-repo-cleaner

--- manifests/profiles/java.pp
+++ manifests/profiles/java.pp
@@ -11,7 +11,6 @@ class jenkins_ci_setup::profiles::java (
   jenkins::plugin { 'm2release': }
   jenkins::plugin { 'javadoc': }
   jenkins::plugin { 'pipeline-maven': }
-  jenkins::plugin { 'maven-repo-cleaner': }
   jenkins::plugin { 'gradle': }
   jenkins::plugin { 'jacoco': }
 
