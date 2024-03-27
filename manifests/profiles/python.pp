c2a1c8870da01575611d7ef18d331d573a7657f8
Amin Dandache <git@amin85.de>
2024-03-27 17:30:39 +0100

    remove virtualenv in packages for pip support

--- manifests/profiles/python.pp
+++ manifests/profiles/python.pp
@@ -4,7 +4,7 @@ class jenkins_ci_setup::profiles::python (
 ) {
   ensure_packages(
     # TODO make sure this gets updated
-    [ "python3-pip", "python3-dev", "virtualenv", "python3", "zlib1g-dev", ]
+    [ "python3-pip", "python3-dev", "python3", "zlib1g-dev", ]
   )
   # https://gist.github.com/kogcyc/07c3e5d1f427c9fa6b99044d81f8ee82
   ensure_packages(['docker-compose', 'pipenv', "black", "isort", "twine" ], {
